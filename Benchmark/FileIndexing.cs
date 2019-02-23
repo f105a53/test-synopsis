02fbd4e132ff0d9f6ac8fd6d339e5c3d558af136
J2ghz <j2.00ghz@gmail.com>
2019-02-23 14:46:33 +0100

    fix code for many files

--- Benchmark/FileIndexing.cs
+++ Benchmark/FileIndexing.cs
@@ -11,21 +11,37 @@ namespace Benchmark
     [RankColumn]
     public class FileIndexing
     {
-        [Params("data/1")] public string path;
+        [Params("data/singlefile", "data/arnold-j")]
+        public string path;
 
         private char[] whitespace;
+
+        private static IEnumerable<FileInfo> Crawl(DirectoryInfo dir)
+        {
+            foreach (var file in dir.EnumerateFiles()) yield return file;
+            foreach (var d in dir.EnumerateDirectories())
+            foreach (var file in Crawl(d))
+                yield return file;
+        }
 
         [Benchmark]
         public List<string> FileReadAllTextSplit()
         {
             var list = new List<string>();
-            foreach (var line in File.ReadAllLines(path))
+            foreach (var file in GetFiles())
+            foreach (var line in File.ReadAllLines(file.FullName))
             {
                 var lineTerms = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries)
                     .Select(s => s.ToLowerInvariant()).ToArray();
                 foreach (var term in lineTerms) list.Add(term);
             }
+
             return list;
+        }
+
+        private IEnumerable<FileInfo> GetFiles()
+        {
+            return Crawl(new DirectoryInfo(path));
         }
 
         [GlobalSetup]
@@ -41,44 +57,46 @@ namespace Benchmark
             whitespace = a.ToArray();
         }
 
-        [Benchmark]
-        public List<string> StreamReaderSlice3rdParty()
-        {
-            var w = new Span<char>(whitespace);
-            var list = new List<string>();
-            using (var sr = new StreamReader(path))
-            {
-                while (!sr.EndOfStream)
-                {
-                    var line = sr.ReadLine().AsSpan().Split(w);
-                    foreach (var word in line) list.Add(word.ToString());
-                }
-            }
+        //[Benchmark]
+        //public List<string> StreamReaderSlice3rdParty()
+        //{
+        //    var w = new Span<char>(whitespace);
+        //    var list = new List<string>();
+        //    foreach (var file in GetFiles())
+        //        using (var sr = new StreamReader(file.FullName))
+        //        {
+        //            while (!sr.EndOfStream)
+        //            {
+        //                var line = sr.ReadLine().AsSpan().Split(w);
+        //                foreach (var word in line) list.Add(word.ToString());
+        //            }
+        //        }
 
-            return list;
-        }
+        //    return list;
+        //}
 
         [Benchmark]
         public List<string> StreamReaderSliceCustom()
         {
             var w = new Span<char>(whitespace);
             var list = new List<string>();
-            using (var sr = new StreamReader(path))
-            {
-                while (!sr.EndOfStream)
+            foreach (var file in GetFiles())
+                using (var sr = new StreamReader(file.FullName))
                 {
-                    var line = sr.ReadLine().AsSpan();
-                    var index = 0;
-                    while (index < line.Length)
+                    while (!sr.EndOfStream)
                     {
-                        var newIndex = line.Slice(index).IndexOf(w);
-                        if (newIndex < index) break;
-                        var term = line.Slice(index, newIndex - index);
-                        list.Add(term.ToString());
-                        index = newIndex + 1;
+                        var line = sr.ReadLine().AsSpan();
+                        var index = 0;
+                        while (index < line.Length)
+                        {
+                            var newIndex = line.Slice(index).IndexOf(w);
+                            if (newIndex < index) break;
+                            var term = line.Slice(index, newIndex - index);
+                            list.Add(term.ToString());
+                            index = newIndex + 1;
+                        }
                     }
                 }
-            }
 
             return list;
         }
@@ -87,15 +105,16 @@ namespace Benchmark
         public List<string> StreamReaderSplit()
         {
             var list = new List<string>();
-            using (var sr = new StreamReader(path))
-            {
-                while (!sr.EndOfStream)
+            foreach (var file in GetFiles())
+                using (var sr = new StreamReader(file.FullName))
                 {
-                    var lineTerms = sr.ReadLine().Split(new char[0], StringSplitOptions.RemoveEmptyEntries)
-                        .Select(s => s.ToLowerInvariant()).ToArray();
-                    foreach (var term in lineTerms) list.Add(term);
+                    while (!sr.EndOfStream)
+                    {
+                        var lineTerms = sr.ReadLine().Split(new char[0], StringSplitOptions.RemoveEmptyEntries)
+                            .Select(s => s.ToLowerInvariant()).ToArray();
+                        foreach (var term in lineTerms) list.Add(term);
+                    }
                 }
-            }
 
             return list;
         }
