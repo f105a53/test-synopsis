6f53d4cf884efe8e41ac0c818a51babbd46634f5
J2ghz <j2.00ghz@gmail.com>
2019-03-11 16:13:46 +0100

    randomly choose next server

--- LoadBalancer/Controllers/SearchController.cs
+++ LoadBalancer/Controllers/SearchController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Common.Data;
 using InfluxDB.Collector;
@@ -11,34 +12,32 @@ namespace LoadBalancer.Controllers
     [ApiController]
     public class SearchController : ControllerBase
     {
-        private readonly LinkedList<RestClient> clients = new LinkedList<RestClient>();
-        private LinkedListNode<RestClient> current;
+        private readonly string[] servers = {"http://serach-api1/api", "http://search-api2/api"};
+        private readonly RestClient current;
 
         public SearchController()
         {
-            clients.AddLast(new RestClient("http://serach-api1/api"));
-            clients.AddLast(new RestClient("http://search-api2/api"));
-            current = clients.First;
+            //Randomly choose a server
+            var i = new Random().Next(0, servers.Length);
+            current = new RestClient(servers[i]);
         }
 
         [HttpGet]
         public async Task<List<TermDoc>> Search([FromQuery] string q)
         {
-            //Use Round-robin to choose next server
-            current = current.Next ?? current.List.First;
-            var client = current;
+            var restClient = current;
             //Report request
             Metrics.Increment("totalRequests", 1,
-                new Dictionary<string, string> {{"server", client.Value.BaseUrl.ToString()}});
+                new Dictionary<string, string> {{"server", restClient.BaseUrl.ToString()}});
 
             var r = new RestRequest("search", Method.GET, DataFormat.Json);
             r.AddQueryParameter("q", q);
             IRestResponse<List<TermDoc>> result;
             //Report request duration
             using (Metrics.Time("serverResponseTime",
-                new Dictionary<string, string> {{"server", client.Value.BaseUrl.ToString()}}))
+                new Dictionary<string, string> {{"server", restClient.BaseUrl.ToString()}}))
             {
-                result = await client.Value.ExecuteTaskAsync<List<TermDoc>>(r);
+                result = await restClient.ExecuteTaskAsync<List<TermDoc>>(r);
             }
 
             return result.Data;
