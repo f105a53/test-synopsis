e2a50cccca6edb9bff665bab93f321262cef8ac4
Jacob Hinze <jacob.hinze@hotmail.com>
2019-05-20 13:32:24 +0200

    Added searchServices

---
+++ SearchAPI/Services/SearchService.cs
@@ -0,0 +1,23 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SearchAPI.Services
+{
+    public class SearchService
+    {
+        private readonly Common.Index index;
+
+        public SearchService(Common.Index index)
+        {
+            this.index = index;
+        }
+
+        public SearchResults GetResults(string term)
+        {
+            return index.Search(term);
+        }
+    }
+}
