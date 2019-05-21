9a30ab4bbd8a82db68b0e4957e365708274fd780
J2ghz <j2.00ghz@gmail.com>
2019-05-21 17:34:41 +0200

    fix email mapping

--- Server/Models/Email.cs
+++ Server/Models/Email.cs
@@ -1,19 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using AutoMapper;
 
 namespace Server.Models
 {
     public class Email
     {
-        public string[] From { get; set; }
-        public string[] To { get; set; }
-        public string[] Cc { get; set; }
         public string[] Bcc { get; set; }
-        public string Subject { get; set; }
-        public DateTimeOffset Date { get; set; }
-        public string Path { get; set; }
         public string Body { get; set; }
+        public string[] Cc { get; set; }
+        public DateTimeOffset Date { get; set; }
+        public string[] From { get; set; }
+        public string Path { get; set; }
+        public string Subject { get; set; }
+        public string[] To { get; set; }
+    }
+
+    public class EmailProfile : Profile
+    {
+        public EmailProfile()
+        {
+            CreateMap<Common.Models.Email, Email>().ForMember(e => e.Body, expression => expression.Ignore());
+        }
     }
 }
