6f6ed78baa2e29d7f01d7490903ab62ace37ca87
J2ghz <j2.00ghz@gmail.com>
2019-05-21 11:34:28 +0200

    Create SerachNode

---
+++ SearchNode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SearchNode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
