8b2562499909483553168aa95107af0fbef8000d
J2ghz <j2.00ghz@gmail.com>
2019-05-21 13:49:26 +0200

    Create PreviewService

---
+++ PreviewService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PreviewService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
