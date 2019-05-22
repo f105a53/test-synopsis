8956d163369f9e8604a137a8921c48b5a096c9e2
Jacob Hinze <jacob.hinze@hotmail.com>
2019-05-22 11:51:27 +0200

    Added search controller removed auto generated.

---
+++ PublicAPI/Controllers/SearchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PublicAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SearchController : ControllerBase
+    {
+
+        // GET: api/Search/5
+        [HttpGet("{query}", Name = "Get")]
+        public SearchResults<Email> Get(string query)
+        {
+            return null;
+        }
+    }
+}
