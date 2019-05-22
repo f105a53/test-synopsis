b77153993fd8cebab14231452f1356af0781ae99
Jacob Hinze <jacob.hinze@hotmail.com>
2019-05-22 12:23:25 +0200

    Added service to controller

--- PublicAPI/Controllers/SearchController.cs
+++ PublicAPI/Controllers/SearchController.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 using Common.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PublicAPI.Models;
+using PublicAPI.Services;
+using Email = PublicAPI.Models.Email;
 
 namespace PublicAPI.Controllers
 {
@@ -12,12 +15,20 @@ namespace PublicAPI.Controllers
     [ApiController]
     public class SearchController : ControllerBase
     {
+        private readonly SearchService _searchService;
+
+        public SearchController(SearchService searchService)
+        {
+            _searchService = searchService;
+        }
 
         // GET: api/Search/query
         [HttpGet("{query}", Name = "Get")]
-        public SearchResults<Email> Get(string query)
+        public async Task<SearchResults<Email>> Get(string query)
         {
-            return null;
+            var searchResults = await _searchService.Search(query);
+
+            return searchResults;
         }
     }
 }
