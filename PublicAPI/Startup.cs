2d6ace4bed6e75097d1be92a56a1cedafb3cca53
Mikkel <mikkelljungberg@gmail.com>
2019-05-22 12:34:32 +0200

    Added automapper and rabbithutch to startup.cs

--- PublicAPI/Startup.cs
+++ PublicAPI/Startup.cs
@@ -10,6 +10,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using PublicAPI.Services;
+using AutoMapper;
+using EasyNetQ;
 
 namespace PublicAPI
 {
@@ -27,11 +30,17 @@ namespace PublicAPI
         {
             services.AddControllers()
                 .AddNewtonsoftJson();
+
+            services.AddAutoMapper(typeof(PublicAPI.Startup).Assembly, typeof(Common.Models.Email).Assembly);
+            services.AddSingleton(RabbitHutch.CreateBus(Environment.GetEnvironmentVariable("RABBITMQ_CSTRING") ?? "host=localhost"));
+            services.AddSingleton<SearchService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMapper mapper)
         {
+            mapper.ConfigurationProvider.AssertConfigurationIsValid();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
