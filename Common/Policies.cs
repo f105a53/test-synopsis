2c811192e83a7c30492c36bb15766611254c8ccb
J2ghz <j2.00ghz@gmail.com>
2019-05-22 15:35:24 +0200

    add cache

---
+++ Common/Policies.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Humanizer;
+using Microsoft.Extensions.Caching.Memory;
+using Polly;
+using Polly.Caching;
+using Polly.Caching.Memory;
+
+namespace Common
+{
+    public static class Policies<T>
+    {
+        public static readonly IAsyncPolicy<T> Cache = Policy.CacheAsync<T>(new MemoryCacheProvider(new MemoryCache(new MemoryCacheOptions())),
+            new SlidingTtl(5.Minutes()));
+
+        public static readonly IAsyncPolicy<T> Complete = Policy.WrapAsync(Cache);
+    }
+}
