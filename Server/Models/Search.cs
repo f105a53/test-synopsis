ee7310f88b9dd5072feaa8388c6f4ce4dbf7dbd6
J2ghz <j2.00ghz@gmail.com>
2019-05-22 12:42:42 +0200

    add spellcheck to server

---
+++ Server/Models/Search.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common.Models;
+
+namespace Server.Models
+{
+    public class Search
+    {
+        public Search(string[] suggestions, SearchResults<Email> emails)
+        {
+            Suggestions = suggestions;
+            Emails = emails;
+        }
+
+        public string[] Suggestions { get; }
+        public SearchResults<Email> Emails { get; }
+
+    }
+}
