47d8acf83998fac9538f7eb9136a221ea9057bb8
Jacob Hinze <kontakt@jacobhinze.dk>
2019-12-12 13:39:11 +0100

    Changed spell check service to use clean architecture

---
+++ SpellCheckService.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using EasyNetQ;
+using EasyNetQ.Logging;
+using SpellCheckService.Core.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SpellCheckService.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread.CurrentThread.Name = "Main";
+            var exitEvent = new ManualResetEvent(false);
+            System.Console.CancelKeyPress += (sender, eventArgs) =>
+            {
+                eventArgs.Cancel = true;
+                exitEvent.Set();
+            };
+
+            LogProvider.SetCurrentLogProvider(ConsoleLogProvider.Instance);
+            var bus = RabbitHutch.CreateBus(Environment.GetEnvironmentVariable("RABBITMQ_CSTRING") ?? "host=localhost");
+
+            bus.RespondAsync<Spellings.Request, Spellings>(request => Task.Factory.StartNew(() =>
+            {
+                var service = new SpellCheckService.Core.Services.SpellCheckService("./lucene-index");
+                return service.GetSpellings(request);
+            }));
+
+            System.Console.WriteLine("Running...\nPress Ctrl+C to exit");
+            exitEvent.WaitOne();
+        }
+    }
+}
